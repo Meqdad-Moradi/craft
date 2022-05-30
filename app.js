2c62967ddb8b683730bfa3c61776ae975c648ae6
meqdad <meqdad.moradi@outlook.com>
2022-05-30 19:22:33 +0200

    some functionality added

--- app.js
+++ app.js
@@ -6,3 +6,32 @@ navLinks.forEach((link) =>
       link.classList.add("active");
    })
 );
+
+// fixed header
+const nav = document.querySelector("#nav");
+
+window.addEventListener("scroll", (e) => {
+   const scrollY = window.scrollY;
+
+   if (scrollY > 0) {
+      nav.classList.add("fixed");
+   } else {
+      nav.classList.remove("fixed");
+   }
+});
+
+// video show & hide
+const videoContainer = document.querySelector("#video-container");
+const playBtn = document.querySelector("#play-btn");
+
+playBtn.addEventListener("click", () => {
+   videoContainer.classList.add("show");
+});
+
+videoContainer.addEventListener("click", (e) => {
+   if (e.target !== e.currentTarget) {
+      return;
+   } else {
+      e.currentTarget.classList.remove("show");
+   }
+});
