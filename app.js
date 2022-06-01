091da524fb6932eb3ba23b49aae78e9e9579d6b2
meqdad <meqdad.moradi@outlook.com>
2022-06-01 23:53:37 +0200

    app js updated

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+// manipulate active status on buttons
+const activeStatus = (myTarget, current) => {
+   myTarget.forEach((a) => a.classList.remove("active"));
+   current.classList.add("active");
+};
+// header menu
 const navLinks = document.querySelectorAll(".nav-list li a");
-
 navLinks.forEach((link) =>
-   link.addEventListener("click", (e) => {
-      navLinks.forEach((a) => a.classList.remove("active"));
-      link.classList.add("active");
+   link.addEventListener("click", () => {
+      activeStatus(navLinks, link);
+   })
+);
+
+// filter works
+const filterBtns = document.querySelectorAll(".filter-btn");
+filterBtns.forEach((btn) =>
+   btn.addEventListener("click", () => {
+      activeStatus(filterBtns, btn);
    })
 );
 
