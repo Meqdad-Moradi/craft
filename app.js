0cd43e468c3d557b7ca82fa9086649cdbb9d7f0b
meqdad <meqdad.moradi@outlook.com>
2022-06-16 11:38:04 +0200

    app.js updated

--- app.js
+++ app.js
@@ -43,3 +43,85 @@ faqTitles.forEach((faq, i) =>
       e.currentTarget.classList.toggle("active");
    })
 );
+
+///////////////////////////////////////////////////////////////////////
+// filter the work section
+
+// create the data
+const datas = [
+   {
+      id: 1,
+      img: "./images/branding.jpg",
+      title: "Branding",
+      subTitle: "Product Branding",
+      category: "branding",
+   },
+   {
+      id: 2,
+      img: "./images/packag-design.jpg",
+      title: "Graphics Design",
+      subTitle: "Packaging Design",
+      category: "Graphics Design",
+   },
+   {
+      id: 3,
+      img: "./images/product-branding.jpg",
+      title: "Branding",
+      subTitle: "Product Branding",
+      category: "branding",
+   },
+   {
+      id: 4,
+      img: "./images/website-mockup.jpg",
+      title: "Web Development",
+      subTitle: "Beauty Care Web Apps",
+      category: "Web Design",
+   },
+   {
+      id: 5,
+      img: "./images/mockup.jpg",
+      title: "Graphics Design",
+      subTitle: "Mocup Design",
+      category: "Graphics Design",
+   },
+   {
+      id: 6,
+      img: "./images/mobile-app.jpg",
+      title: "App Development",
+      subTitle: "Mobile Apps Design",
+      category: "Marketing",
+   },
+];
+
+// select and manipulating data
+const worksCard = document.querySelector("#works-card");
+worksCard.innerHTML = "";
+
+const readDatas = () => {
+   const works = datas
+      .map((item) => {
+         const { id, img, title, subTitle, category } = item;
+         return `
+      <article class="card">
+         <div class="img-box">
+            <img
+               src=${img}
+               alt=${title}
+            />
+         </div>
+         <div class="details">
+            <p>${title}</p>
+            <h3>${subTitle}</h3>
+         </div>
+      </article>`;
+      })
+      .join(" ");
+
+   worksCard.innerHTML = works;
+};
+
+// 
+
+window.addEventListener('DOMContentLoaded', ()=>{
+   readDatas()
+})
